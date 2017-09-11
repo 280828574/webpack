cf30731b09fa4ff22b44b9514ade30d5942bdac9
zero1898 <zero1898x@gmail.com>
2017-09-11 14:59:15 +0800

    添加全局常量

--- template/src/main.js
+++ template/src/main.js
@@ -3,9 +3,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
 import '~styles';
-import '~constant';
 import Vue from 'vue';
 import api from '~api';
+import constant from '~constant';
 {{#store}}
 import store from '~store';
 {{/store}}
@@ -19,6 +19,7 @@ import App from './App';
 
 Vue.config.productionTip = false;
 Vue.prototype.$api = api;
+Vue.prototype.$const = constant;
 {{#mobile}}
 FastClick.attach(document.body);
 {{/mobile}}
