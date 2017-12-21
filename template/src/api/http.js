702f9e1145e93752062f801558462a6e5802822c
zero1898 <zero1898x@gmail.com>
2017-12-21 17:54:26 +0800

    axios

--- template/src/api/http.js
+++ template/src/api/http.js
@@ -1,58 +1,61 @@
 import axios from './axios';
 
 /**
- * axios实现get
- * @param {*Object} conf axios的config
+ * axios实现post
  */
-const get = async (url, params) => {
-    let waitData = await axios({
+const fetch = (url, options = {}) => {
+    let axiosConf = {
+        url: url
+    };
+    if (options.method && options.method.toLowerCase() === 'post') {
+        // 如果是POST请求，初始化参数
+        axiosConf.method = 'post';
+        axiosConf.data = options.body || {};
+    } else {
+        // 不是POST请求就是GET请求！初始化参数
+        axiosConf.method = 'get';
+        axiosConf.params = options.body || {};
+    }
+    return axios(axiosConf)
+        .then(res => {
+            res.data.success = true;
+            return res.data;
+        })
+        .catch(err => {
+            err.data || (err.data = {});
+            if (err.data.msg) {
+                console.log('fetchError =>', err.data.msg);
+            } else {
+                console.log('fetchError =>', err);
+                err.data.msg = '系统错误！';
+            }
+            err.data.success = false;
+            return err.data;
+        });
+};
+
+/**
+ * axios实现get
+ */
+const get = (url, params = {}) => {
+    return fetch(url, {
         method: 'get',
-        url: url,
-        params: params
-    }).then(res => {
-        res.data.success = true;
-        return res.data;
-    }).catch(err => {
-        err.data || (err.data = {});
-        if (err.data.msg) {
-            console.log('getError =>', err.data.msg);
-        } else {
-            console.log('getError =>', err);
-            err.data.msg = '系统错误！';
-        }
-        err.data.success = false;
-        return err.data;
+        body: params
     });
-    return waitData;
 };
 
 /**
  * axios实现post
- * @param {*Object} conf axios的config
  */
-const post = async (url, data) => {
-    let waitData = await axios({
+const post = (url, data = {}) => {
+    return fetch(url, {
         method: 'post',
-        url: url,
-        data: data
-    }).then(res => {
-        res.data.success = true;
-        return res.data;
-    }).catch(err => {
-        err.data || (err.data = {});
-        if (err.data.msg) {
-            console.log('postError =>', err.data.msg);
-        } else {
-            console.log('postError =>', err);
-            err.data.msg = '系统错误！';
-        }
-        err.data.success = false;
-        return err.data;
+        body: data
     });
-    return waitData;
 };
 
 export default {
+    fetch,
     get,
     post
 };
